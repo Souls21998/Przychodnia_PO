16022222c1d4209d11c3e00be355e26c9699c7a9
Souls21998 <pablook11513@gmail.com>
2019-12-13 15:38:03 +0100

    Add new class

---
+++ Przychodnia_PO/Lekarz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przychodnia_PO
+{
+    class Lekarz : Osoba
+    {
+        Specjalizacja specjalizacja;
+        int nr_gabinetu;
+
+        public Lekarz(Specjalizacja specjalizacja, int nr_gabinetu) : base (imie, nazwisko, wiek, ID, nr_telefonu)
+        {
+            this.specjalizacja = specjalizacja;
+            this.nr_gabinetu = nr_gabinetu;
+        }
+    }
+
+    enum Specjalizacja{
+        lekarz_rodziny, kardiolog, psychiatra, ginekolog, neurolog, ortopeda
+    };
+}
---
+++ Przychodnia_PO/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przychodnia_PO
+{
+    abstract class Osoba
+    {
+        protected string imie;
+        protected string nazwisko;
+        protected int wiek;
+        protected int ID;
+        protected int nr_telefonu;
+
+        public Osoba(string imie, string nazwisko, int wiek, int ID, int nr_telefonu)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.wiek = wiek;
+            this.ID = ID;
+            this.nr_telefonu = nr_telefonu;
+        }
+    }
+}
